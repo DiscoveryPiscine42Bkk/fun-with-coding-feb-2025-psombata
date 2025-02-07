feac398bdcc4108ff5d5f65bc8f38cfba755d42a
psombata <64011217@kmitl.ac.th>
2025-02-07 23:49:27 +0700

    Add files via upload

---
+++ rush/psombata.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const description = document.getElementById("description");
+    if (description) {
+        description.textContent = "Hello, I'm Poonsup Sombatanantakorn, and this is my portfolio!";
+    }
+});
